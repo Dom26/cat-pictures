5fad21b31e0cc1e9add80837eaeb71607fd62bd7
Dom26 <domniniquemoore12@yahoo.com>
2022-09-09 20:49:37 -0700

    Successfully calls API and saves image to file in local directory

--- mySampleApp.py
+++ mySampleApp.py
@@ -1,1 +1,19 @@
-print("Hello World")
+import requests
+import shutil
+
+def main():
+    url = "https://cataas.com/c"
+    filename = "fileImage.jpg"
+    response = requests.get(f"{url}", stream=True)
+    if response.status_code == 200:
+        print("data fetched successfully")
+        response.raw.decode_content = True
+        with open(filename,'wb') as f:
+            shutil.copyfileobj(response.raw,f)
+        
+        print(f"image saved to {filename} successfully")
+    else:
+        print(f"There was a problem fetching the data. Code {response.status_code} was returned")
+
+if __name__ == "__main__":
+    main()
